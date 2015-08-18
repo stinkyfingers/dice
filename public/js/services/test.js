37c4e6bb42928c7a32fc43243deb3ac68cace6c6
stinkyfingers <john_shenk@hotmail.com>
2015-08-17 23:12:58 -0500

    deletes extra files

--- public/js/services/test.js
+++
@@ -1,9 +0,0 @@
-define(['app'], function (app) {
-	app.factory("testFactory",function($q, $http, $rootScope){
-		var factory = {};
-		factory.testGet = function(){
-			return "fun";
-		}
-		return factory;
-	});
-});
